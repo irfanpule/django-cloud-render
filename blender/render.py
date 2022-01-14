cb7e7d4d7a634bc1281d7da5a1dfa388ca81c4b7
irfanpule <irfan.biankalang@gmail.com>
2022-01-14 11:00:06 +0700

    fix method _remove_result_render

--- blender/render.py
+++ blender/render.py
@@ -10,6 +10,7 @@ from blender.models import Project
 class BlenderRender:
     log_dir = settings.RENDER_LOG_DIR
     output_dir = settings.OUTPUT_RENDER
+    output_dir_filename = ""
 
     def __init__(self, project: Project, start_frame: int = 0, end_frame: int = 0,
                  option_cycles: str = "CPU", total_thread: int = 2) -> None:
@@ -19,11 +20,12 @@ class BlenderRender:
         self.end_frame = end_frame
         self.option_cycles = option_cycles
         self.total_thread = total_thread
-        self.output_dir = self._generate_output_dir()
+        self._prepare_output_dir()
 
-    def _generate_output_dir(self):
+    def _prepare_output_dir(self):
         output_project_dir = os.path.join(self.output_dir, self.project.slug)
-        return output_project_dir + "/" + self.project.slug
+        self.output_dir = output_project_dir
+        self.output_dir_filename = output_project_dir + "/" + self.project.slug
 
     def _prepare_log_dir(self):
         """
@@ -96,7 +98,7 @@ class BlenderRender:
         self.project.save()
 
         args = shlex.split(
-            f"blender -b {self.filepath} -o {self.output_dir} -s {self.start_frame} -e {self.end_frame} -t {self.total_thread} -a -- --cycles-device {self.option_cycles}")
+            f"blender -b {self.filepath} -o {self.output_dir_filename} -s {self.start_frame} -e {self.end_frame} -t {self.total_thread} -a -- --cycles-device {self.option_cycles}")
         proc_render = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
         self._output_reader(proc_render)
 
